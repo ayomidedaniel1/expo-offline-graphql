56524b33c863bf994597b8768e60a7a703fc7238
Ayomide <ayomidedans@gmail.com>
2025-05-15 02:03:46 +0100

    refractor: added try block to apollo client code

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -7,21 +7,21 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { persistCache } from 'apollo3-cache-persist';
 import { useEffect, useState } from 'react';
 
-// Create the Apollo cache
 const cache = new InMemoryCache();
-
-// Point to demo GraphQL API
 const link = createHttpLink({ uri: 'https://graphqlzero.almansi.me/api' });
 
-// Custom hook to initialize ApolloClient with persisted cache
 export const useApollo = () => {
   const [client, setClient] = useState<ApolloClient<any> | null>(null);
 
   useEffect(() => {
     (async () => {
-      await persistCache({ cache, storage: AsyncStorage });
-      const apolloClient = new ApolloClient({ link, cache });
-      setClient(apolloClient);
+      try {
+        await persistCache({ cache, storage: AsyncStorage });
+        const apolloClient = new ApolloClient({ link, cache });
+        setClient(apolloClient);
+      } catch (e) {
+        console.error('Apollo cache persistence failed:', e);
+      }
     })();
   }, []);
 
